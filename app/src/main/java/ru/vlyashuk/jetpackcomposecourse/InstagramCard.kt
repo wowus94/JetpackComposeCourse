787e53ff51653ebe7fb24bb150d37df98e969f5b
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2025-02-24 22:30:32 +0300

    Practice Card

--- app/src/main/java/ru/vlyashuk/jetpackcomposecourse/InstagramCard.kt
+++ app/src/main/java/ru/vlyashuk/jetpackcomposecourse/InstagramCard.kt
@@ -1,5 +1,6 @@
 package ru.vlyashuk.jetpackcomposecourse
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,22 +25,33 @@ import androidx.compose.ui.unit.dp
 @Preview(showBackground = true)
 @Composable
 fun ProfileCard() {
-    Row(
+    Card(
         modifier = Modifier
             .fillMaxWidth()
+            .height(300.dp)
             .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+        shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
+        colors = CardDefaults.cardColors(Color.White),
+        border = BorderStroke(1.dp, color = Color.Black)
     ) {
-        Box(
+
+        Row(
             modifier = Modifier
-                .size(50.dp)
-                .background(color = Color.Blue)
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(color = Color.Blue)
+            ) {
+            }
+            TitleBoxes()
+            TitleBoxes()
+            TitleBoxes()
         }
-       TitleBoxes()
-       TitleBoxes()
-       TitleBoxes()
     }
 }
 
