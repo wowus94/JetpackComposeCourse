c01b0281b242cd5d449c7f9c4442f60e96835c26
Sr_Lyashuk <sesila94@mail.ru>
2025-02-25 11:30:20 +0300

    Completion InstagramCard

--- app/src/main/java/ru/vlyashuk/jetpackcomposecourse/InstagramCard.kt
+++ app/src/main/java/ru/vlyashuk/jetpackcomposecourse/InstagramCard.kt
@@ -1,16 +1,19 @@
 package ru.vlyashuk.jetpackcomposecourse
 
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -18,7 +21,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,13 +38,11 @@ fun ProfileCard() {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(300.dp)
             .padding(8.dp),
         shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
         colors = CardDefaults.cardColors(MaterialTheme.colorScheme.background),
         border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.onBackground)
     ) {
-
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -45,15 +50,45 @@ fun ProfileCard() {
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Box(
+            Image(
                 modifier = Modifier
-                    .size(50.dp)
-                    .background(color = Color.Blue)
-            ) {
-            }
+                    .size(58.dp)
+                    .clip(shape = CircleShape)
+                    .background(color = Color.White)
+                    .padding(4.dp),
+                painter = painterResource(id = R.drawable.ic_favorite),
+                contentDescription = ""
+            )
             UsersStatistics("Posts", "123")
             UsersStatistics("Followers", "44M")
             UsersStatistics("Following", "70")
+        }
+        Text(
+            text = "Instagramm",
+            modifier = Modifier
+                .padding(top = 40.dp, start = 16.dp),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 24.sp
+        )
+        Text(
+            text = "#Hashtag",
+            modifier = Modifier.padding(start = 16.dp),
+            fontStyle = FontStyle.Italic,
+            fontSize = 18.sp
+        )
+        Text(
+            text = "www.sitetest.com",
+            modifier = Modifier.padding(16.dp),
+            fontFamily = FontFamily.Serif
+        )
+        val context = LocalContext.current
+        Button(
+            onClick = {
+                Toast.makeText(context, "Hello Click", Toast.LENGTH_SHORT).show()
+            },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Click Me")
         }
     }
 }
