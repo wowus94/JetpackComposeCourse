014d7cb1b6b8ed5ee062665837123ed75611da52
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2025-03-25 22:17:14 +0300

    Setting Flow

--- app/src/main/java/ru/vlyashuk/jetpackcomposecourse/vk_app/presentation/news/NewsFeedScreen.kt
+++ app/src/main/java/ru/vlyashuk/jetpackcomposecourse/vk_app/presentation/news/NewsFeedScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,7 +32,7 @@ fun NewsFeedScreen(
 ) {
     val viewModel: NewsFeedViewModel = viewModel()
 
-    val screenState = viewModel.screenState.observeAsState(NewsFeedScreenState.Initial)
+    val screenState = viewModel.screenState.collectAsState(NewsFeedScreenState.Initial)
 
     when (val currentState = screenState.value) {
         is NewsFeedScreenState.Posts -> {
